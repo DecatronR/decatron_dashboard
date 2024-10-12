7a02d2e9d4a03596c3962201021f7561e20faa56
koladeolukoya <koladeolukoya@gmail.com>
2024-10-13 00:54:28 +0100

    ref: add role title

--- src/layouts/dashboard/config.js
+++ src/layouts/dashboard/config.js
@@ -1,27 +1,27 @@
-import ChartBarIcon from '@heroicons/react/24/solid/ChartBarIcon';
-import CogIcon from '@heroicons/react/24/solid/CogIcon';
-import LockClosedIcon from '@heroicons/react/24/solid/LockClosedIcon';
-import ShoppingBagIcon from '@heroicons/react/24/solid/ShoppingBagIcon';
-import UserIcon from '@heroicons/react/24/solid/UserIcon';
-import UserPlusIcon from '@heroicons/react/24/solid/UserPlusIcon';
-import PlusCircleIcon from '@heroicons/react/24/solid/PlusCircleIcon';
-import UsersIcon from '@heroicons/react/24/solid/UsersIcon';
-import WrenchIcon from '@heroicons/react/24/solid/WrenchIcon';
-import { SvgIcon } from '@mui/material';
+import ChartBarIcon from "@heroicons/react/24/solid/ChartBarIcon";
+import CogIcon from "@heroicons/react/24/solid/CogIcon";
+import LockClosedIcon from "@heroicons/react/24/solid/LockClosedIcon";
+import ShoppingBagIcon from "@heroicons/react/24/solid/ShoppingBagIcon";
+import UserIcon from "@heroicons/react/24/solid/UserIcon";
+import UserPlusIcon from "@heroicons/react/24/solid/UserPlusIcon";
+import PlusCircleIcon from "@heroicons/react/24/solid/PlusCircleIcon";
+import UsersIcon from "@heroicons/react/24/solid/UsersIcon";
+import WrenchIcon from "@heroicons/react/24/solid/WrenchIcon";
+import { SvgIcon } from "@mui/material";
 
 export const items = [
   {
-    title: 'Overview',
-    path: '/',
+    title: "Overview",
+    path: "/",
     icon: (
       <SvgIcon fontSize="small">
         <ChartBarIcon />
       </SvgIcon>
-    )
+    ),
   },
   {
-    title: 'User Management',
-    path: '#',
+    title: "User Management",
+    path: "#",
     icon: (
       <SvgIcon fontSize="small">
         <UsersIcon />
@@ -29,18 +29,18 @@ export const items = [
     ),
     subItems: [
       {
-        title: 'Users',
-        path: '/users',
+        title: "Users",
+        path: "/users",
       },
       {
-        title: 'Add Role',
-        path: '/roles',
+        title: "Roles",
+        path: "/roles",
       },
-    ]
+    ],
   },
   {
-    title: 'Listing Configuration',
-    path: '#',
+    title: "Listing Configuration",
+    path: "#",
     icon: (
       <SvgIcon fontSize="small">
         <WrenchIcon />
@@ -48,39 +48,39 @@ export const items = [
     ),
     subItems: [
       {
-        title: 'Listing Type',
-        path: '/listing-types',
+        title: "Listing Type",
+        path: "/listing-types",
       },
       {
-        title: 'Property Type',
-        path: '/property-types',
+        title: "Property Type",
+        path: "/property-types",
       },
       {
-        title: 'Property Usage',
-        path: '/property-usage-types',
+        title: "Property Usage",
+        path: "/property-usage-types",
       },
       {
-        title: 'Property Condition',
-        path: '/property-conditions',
+        title: "Property Condition",
+        path: "/property-conditions",
       },
       {
-        title: 'Add State',
-        path: '/states',
+        title: "Add State",
+        path: "/states",
       },
       {
-        title: 'Add LGA',
-        path: '/local_governments',
+        title: "Add LGA",
+        path: "/local_governments",
       },
-    ]
+    ],
   },
   {
-    title: 'Create Listing',
-    path: '/create-listing',
+    title: "Create Listing",
+    path: "/create-listing",
     icon: (
       <SvgIcon fontSize="small">
         <PlusCircleIcon />
       </SvgIcon>
-    )
+    ),
   },
   // {
   //   title: 'Favorites',
@@ -101,13 +101,13 @@ export const items = [
   //   )
   // },
   {
-    title: 'My Profile',
-    path: '/account',
+    title: "My Profile",
+    path: "/account",
     icon: (
       <SvgIcon fontSize="small">
         <UserIcon />
       </SvgIcon>
-    )
+    ),
   },
   // {
   //   title: 'Settings',
